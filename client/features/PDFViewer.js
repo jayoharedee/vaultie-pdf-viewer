2dccca2ec7e24aa6874bbcfc1a86dee0948653b7
jayoharedee <jmorenstein@georgebrown.ca>
2020-07-22 09:40:25 -0400

    added page change handlers for pagination

--- client/features/PDFViewer.js
+++ client/features/PDFViewer.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react'
-import {
-  Document,
-  Page,
-  Text,
-} from 'react-pdf/dist/entry.webpack'
+import { Document, Page } from 'react-pdf/dist/entry.webpack'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import PropTypes from 'prop-types'
 
-// import './Sample.less'
+import Button from '../components/Button'
 
 const options = {
   cMapUrl: 'cmaps/',
@@ -16,8 +12,6 @@ const options = {
 
 function PDFViewer({
   file,
-  onLoadSuccess,
-  onLoadError,
   onFileChange,
 }) {
   const [numPages, setNumPages] = useState(null)
@@ -25,43 +19,59 @@ function PDFViewer({
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
+    console.log('numpages', numPages)
   }
 
-  function onFileChange(event) {
-    setFile(event.target.files[0])
+  function handleNextPage() {
+    if (numPages === pageNumber - 1) {
+      return false
+    }
+
+    const currentPage = pageNumber + 1
+    setPageNumber(currentPage)
+  }
+
+  function handlePreviousPage() {
+    if (pageNumber === 1) {
+      return false
+    }
+    setPageNumber(pageNumber - 1)
   }
 
   return (
-    <Document
-      file={file}
-      onLoadSuccess={onDocumentLoadSuccess}
-      onLoadError={console.error}
-      onFileChange={onFileChange}
-      options={options}
-    >
-      {Array.from(new Array(numPages), (el, index) => {
-        if (numPages === pageNumber - 1) {
-          setPageNumber(1)
-        }
-        if (index === 0) {
-          return (
-            <Page
-              key={`page_${index + 1}`}
-              pageNumber={pageNumber}
-              pageIndex={index}
-            />
-          )
-        }
-      })}
-    </Document>
+    <>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={console.error}
+        onFileChange={onFileChange}
+        options={options}
+      >
+        {Array.from(new Array(numPages), (el, index) => {
+          if (numPages === pageNumber - 1) {
+            setPageNumber(1)
+          }
+          if (index === 0) {
+            return (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={pageNumber}
+                pageIndex={index}
+              />
+            )
+          }
+        })}
+      </Document>
+      <Button onClick={handleNextPage} title={'Next Page'} />
+      <Button onClick={handlePreviousPage} title={'Previous Page'} />
+    </>
   )
 }
 
 PDFViewer.propTypes = {
   file: PropTypes.string.isRequired,
-  onLoadError: PropTypes.func.isRequired,
-  numPages: PropTypes.number.isRequired,
-  pageNumber: PropTypes.number.isRequired,
+  setFile: PropTypes.func.isRequired,
+  onFileChange: PropTypes.func.isRequired,
 }
 
 export default PDFViewer
