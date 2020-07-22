220b76458a0a0aa86ae8d185aae6ea98a7600564
jayoharedee <jmorenstein@georgebrown.ca>
2020-07-22 09:35:52 -0400

    included PDFViewer component

--- client/App.js
+++ client/App.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react'
-import {
-  Document,
-  Page,
-} from 'react-pdf/dist/entry.webpack'
-import 'react-pdf/dist/Page/AnnotationLayer.css'
 
 import PDFViewer from './features/PDFViewer'
 import './Sample.less'
 
 function App() {
   const [file, setFile] = useState('./sample.pdf')
-  const [numPages, setNumPages] = useState(0)
-  const [pageNumber, setPageNumber] = useState(1)
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages)
-  }
 
   function onFileChange(event) {
     setFile(event.target.files[0])
@@ -34,34 +23,10 @@ function App() {
         <div className="PDF__container__document">
           <PDFViewer
             file={file}
-            onLoadError={console.error}
-            numPages={numPages}
-            pageNumber={pageNumber}
+            setFile={setFile}
+            onFileChange={onFileChange}
           />
         </div>
-        <button
-          onClick={() => {
-            if (numPages === pageNumber - 1) {
-              return false
-            }
-
-            const currentPage = pageNumber + 1
-            setPageNumber(currentPage)
-          }}
-        >
-          Next page
-        </button>
-
-        <button
-          onClick={() => {
-            if (pageNumber === 1) {
-              return false
-            }
-            setPageNumber(pageNumber - 1)
-          }}
-        >
-          Previous page
-        </button>
       </div>
     </div>
   )
